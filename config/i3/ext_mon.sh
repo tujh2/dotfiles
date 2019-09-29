88a6e2d5337ee0c8311453ae56633128d83e2a8d
Egor Sysoykin <esisoykin@gmail.com>
2019-09-29 20:06:38 +0300

    ext_mon.sh fixed

--- config/i3/ext_mon.sh
+++ config/i3/ext_mon.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "-p" ]]; then
 	xrandr --output HDMI1 --mode 1920x1080 $2 eDP1
 elif [[ "$1" == "-r" ]]; then
-	xrandr --output HDMI1 --rotate $
+	xrandr --output HDMI1 --rotate $2
 elif [[ "$1" == "--off"  ]]; then
 	xrandr --output HDMI1 --off
 fi
