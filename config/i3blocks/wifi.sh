6425c59f5bc14f968617aa054c18769e56fcf12b
Egor Sysoykin <esisoykin@gmail.com>
2019-10-01 21:58:24 +0300

    added icons

--- config/i3blocks/wifi.sh
+++ config/i3blocks/wifi.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [[ "$(nmcli r wifi)"  == "disabled" ]]; then
-	echo " <span foreground=\"#FF0000\">OFF</span>"
+	echo " <span foreground=\"#FF0000\">DONW</span>"
 else
 	SSID=$(nmcli -m tabular -t -f GENERAL.CONNECTION d show wlp1s0)
-	IP=$(nmcli -m tabular -t -f IP4.ADDRESS d show wlp1s0 | awk -F/ '{print $1}')	
+	IP=$(nmcli -m tabular -t -f IP4.ADDRESS d show wlp1s0 | awk -F/ '{print $1}')
+	QUALITY=$(grep wlp1s0 /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
+	
 	if [[ "$SSID" == "" ]]; then
 		echo " <span foreground=\"#00FFFF\">NONE</span>"
 	else
-		echo " <span foreground=\"#00FF00\">$SSID</span> ($IP)"
+		echo " <span foreground=\"#00FF00\">($QUALITY% at $SSID)</span> <span foreground=\"#91E78B\">$IP</span>"
 	fi
 fi
