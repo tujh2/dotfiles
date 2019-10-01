de7d78a06660abd063d7c7a3e48a52b57567c6ec
Egor Sysoykin <esisoykin@gmail.com>
2019-10-01 22:13:22 +0300

    fixed volume format

--- config/i3blocks/volume.sh
+++ config/i3blocks/volume.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ "$(pamixer --get-mute)" == "true"  ]]; then 
-	echo " <span foreground=\"#FF0000\">muted</span> ($(pamixer --get-volume) %)"
+	echo " <span foreground=\"#FF0000\">(muted)</span> $(pamixer --get-volume) %"
 else echo "  $(pamixer --get-volume) %"
 fi
