558f4fb0e366a8438d6a14028c8ab52acc9f4e4e
Egor Sysoykin <esisoykin@gmail.com>
2019-10-01 22:03:29 +0300

    battery fixes

--- config/i3blocks/battery.sh
+++ config/i3blocks/battery.sh
@@ -5,18 +5,18 @@ bat_now=$(cat /sys/class/power_supply/BAT0//uevent | grep -oP 'POWER_SUPPLY_CAPA
 
 if [[ "$bat_status" == "Discharging" ]]; then
 	if [[ "$bat_now"  -lt "20" ]]; then
-		echo "  <span foreground=\"#FF0000\"> $bat_status</span> $bat_now %"
+		echo "<span foreground=\"#FF0000\">  </span> $bat_now %"
 	elif [[ "$bat_now" -lt "40" ]]; then 
-		echo "  <span foreground=\"#FFAE00\"> $bat_status</span> $bat_now %"
+		echo "<span foreground=\"#FFAE00\">  </span> $bat_now %"
 	elif [[ "$bat_now" -lt "60"  ]]; then
-		echo "  <span foreground=\"#FFF600\"> $bat_status</span> $bat_now %"
+		echo "<span foreground=\"#FFF600\">  </span> $bat_now %"
 	elif [[ "$bat_now" -lt "85" ]]; then 
-		echo "  <span foreground=\"#A8FF00\"> $bat_status</span> $bat_now %"
+		echo "<span foreground=\"#A8FF00\">  </span> $bat_now %"
 	else 
-		echo "  <span foreground=\"#00FF00\"> $bat_status</span> $bat_now %"
+		echo "<span foreground=\"#00FF00\">  </span> $bat_now %"
 	fi
 elif [[ "$bat_status" == "Charging" ]]; then
-	echo " <span foreground=\"#00FF00\"> $bat_status $bat_now %</span>"
+	echo " <span foreground=\"#00FF00\"> $bat_now %</span>"
 elif [[ "$bat_status" == "Full"  ]]; then
 	echo "  <span foreground=\"#00FF00\"> FULL</span>"
 fi
