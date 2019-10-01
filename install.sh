4fe2973d747d1cef5627dd30057e0b014bc05a1c
Egor Sysoykin <esisoykin@gmail.com>
2019-10-01 22:16:06 +0300

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.config
 chmod +x ~/dotfiles/config/i3/*.sh
+chmod +x ~/dotfiles/config/i3blocks/*.sh
 ln -s ~/dotfiles/config/* ~/.config
 ln -s ~/.i3blocks.conf ~/
 ln -s ~/.gtkrc-2.0 ~/
